afa309d59460756e48f5511e87acb66784e0887d
icgemu <yuanrong303@yeah.net>
2017-06-09 14:41:41 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var es = require('elasticsearch');
+
+var es_client = new es.Client({
+	hosts:['localhost:9200'],
+   log:['info','debug']
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+/* GET home page. */
+router.get('/q', function(req, res, next) {
+
+  es_client.
   res.render('index', { title: 'Express' });
 });
 
